853e49bcff10e80d9f04003c0fd2c3aefc52b365
46288袁阳 <&>
2017-01-10 16:29:31 +0800

    去除ready事件

--- pageDiv.js
+++ pageDiv.js
@@ -1,27 +1,7 @@
-document.ready = function (callback) {
-    ///兼容FF,Google
-    if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', function () {
-            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
-            callback();
-        }, false)
-    }
-    //兼容IE
-    else if (document.attachEvent) {
-        document.attachEvent('onreadytstatechange', function () {
-            if (document.readyState == "complete") {
-                document.detachEvent("onreadystatechange", arguments.callee);
-                callback();
-            }
-        })
-    }
-    else if (document.lastChild == document.body) {
-        callback();
-    }
-};
 
-document.ready(function () {
-    !(function (g) {
+
+
+   !(function (g) {
         var pageDiv = function (nowNum, option, callback) {
             this.option = {
                 perNum: 5,
@@ -290,7 +270,6 @@ document.ready(function () {
         });
 
     })(window);
-});
 
 
 
